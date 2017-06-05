07d76f2349a5832a21d93d86e6a03b2c7a73788b
q86643 <q86643@gmail.com>
2017-06-05 17:35:39 +0800

    Add files via upload

---
+++ Slave.ino
@@ -0,0 +1,41 @@
+#include <Wire.h>
+#include <LiquidCrystal.h>
+#include <dht.h> 
+#define dht_dpin 2
+dht DHT;   
+const int SLAVE_ADDRESS = 1;
+char buffer[255];
+
+volatile int temp;
+volatile int hum;
+
+void setup(){
+pinMode(13, OUTPUT);
+Serial.begin(9600);
+Serial.println("Humidity and temperature\n");  
+digitalWrite(4, HIGH);
+digitalWrite(7, LOW);
+
+Wire.begin(SLAVE_ADDRESS); // join I2C bus as a slave with addres
+Wire.onRequest(requestEvent); // register event
+}
+
+void loop() { 
+DHT.read11(dht_dpin); //讀取DHT-11
+
+delay(500);            //每500ms更新一次   
+temp = (int) DHT.temperature;
+hum = (int) DHT.humidity;
+Serial.print(temp);  
+Serial.print("  ");
+Serial.print(hum); 
+Serial.print("\n");
+}
+void requestEvent() {
+//digitalWrite(13,HIGH);
+
+sprintf(buffer,"%d-%d", temp, hum);//將溫濕度讀值輸出
+
+Wire.write(buffer); //將溫濕度讀值輸出
+}
+
---
+++ master.ino
@@ -0,0 +1,34 @@
+#include <dht.h>   
+#define dht_dpin 2 //定義訊號要從Pin進來  
+#include <LiquidCrystal.h>
+#include <Wire.h>
+const int SLAVE_ADDRESS = 1;
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+dht DHT;  
+char incomingByte = 0;
+  
+void setup(){ 
+Wire.begin(); 
+lcd.begin(20, 2); 
+Serial.begin(9600);  //Let system settle   
+delay(300);           
+}
+  
+void loop(){
+Wire.requestFrom(SLAVE_ADDRESS, 255);
+
+//DHT.read11(dht_dpin);   //去library裡面找DHT.read11  
+//Serial.print("Humidity = ");   
+//Serial.print(DHT.humidity);  
+//Serial.print("% ");   
+//Serial.print("temperature = ");   
+//Serial.print(DHT.temperature);   
+//Serial.println("C ");
+
+while (Wire.available()) {
+  incomingByte = Wire.read(); // receive a byte
+  lcd.print(incomingByte);  // Display at LCD 
+}
+delay(500);
+lcd.clear();
+}  
